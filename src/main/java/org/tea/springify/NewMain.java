e10bebdfa734643605ae29bf5434e6a95a4c87a3
Jan-Hendrik Alves <j.alves@mwaysolutions.com>
2018-02-02 13:59:35 +0100

    Reformat, changes with assertions, treatment of expressions if blob

--- src/main/java/org/tea/springify/NewMain.java
+++ src/main/java/org/tea/springify/NewMain.java
@@ -1,137 +1,150 @@
 package org.tea.springify;
 
-import com.github.javaparser.JavaParser;
-import com.github.javaparser.ast.CompilationUnit;
-import com.github.javaparser.ast.Node;
-import com.github.javaparser.ast.NodeList;
-import com.github.javaparser.ast.body.*;
-import com.github.javaparser.ast.expr.AnnotationExpr;
-import com.github.javaparser.ast.expr.AssignExpr;
-import com.github.javaparser.ast.expr.Expression;
-import com.github.javaparser.ast.expr.NameExpr;
-import com.github.javaparser.ast.stmt.BlockStmt;
-import com.github.javaparser.ast.stmt.ExpressionStmt;
-import com.github.javaparser.ast.stmt.Statement;
-
+import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import com.github.javaparser.JavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.NodeList;
+import com.github.javaparser.ast.body.BodyDeclaration;
+import com.github.javaparser.ast.body.ConstructorDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.body.Parameter;
+import com.github.javaparser.ast.body.TypeDeclaration;
+import com.github.javaparser.ast.expr.AnnotationExpr;
+import com.github.javaparser.ast.expr.NameExpr;
+import com.github.javaparser.ast.stmt.BlockStmt;
+import com.github.javaparser.ast.stmt.Statement;
+
+
 public class NewMain {
 
-    public static void main(String[] args) throws FileNotFoundException {
-        assert args.length == 1 : "give exactly on java file";
-        assert args[0].endsWith(".java") : "only java files allowed, but filename is " + args[0];
+	public static void main(final String[] args) throws Exception {
+		if (args.length != 1) {
+			System.out.println("give exactly one parameter");
+			System.exit(1);
+		}
+		if (!new File(args[0]).exists()) {
+			System.out.println("the file " + args[0] + " does not exist");
+		}
+		final FileInputStream in = new FileInputStream(args[0]);
+		final CompilationUnit cu = JavaParser.parse(in);
 
-        // creates an input stream for the file to be parsed
-        FileInputStream in = new FileInputStream(args[0]);
+		changeToConstructorInjection(cu);
+		System.out.println(cu.toString());
+		// TODO write output -> maybe to same file
+	}
 
-        // parse the file
-        CompilationUnit cu = JavaParser.parse(in);
+	private static void changeToConstructorInjection(final CompilationUnit cu) throws Exception {
+		boolean optionalImportNeeded = false;
+		final List<MethodDeclaration> nodesToRemove = new ArrayList<>();
+		// Go through all the types in the file
+		final NodeList<TypeDeclaration<?>> types = cu.getTypes();
+		for (final TypeDeclaration<?> type : types) {
+			// Set Type and Autowired for the constructor
+			Node firstSetter = null;
+			final ConstructorDeclaration constructorDeclaration = new ConstructorDeclaration();
+			constructorDeclaration.setName(type.getName());
+			constructorDeclaration.addAnnotation("Autowired");
 
-        changeToConstructorInjection(cu);
+			final NodeList<BodyDeclaration<?>> members = type.getMembers();
+			for (final BodyDeclaration<?> member : members) {
+				if (member instanceof MethodDeclaration) {
+					final MethodDeclaration method = (MethodDeclaration) member;
+					// only go for annotated methods
+					if (containsString(method.getAnnotations(), "@Autowired")) {
+						// special treatment for non required autowirings
+						final boolean optionalNeeded = (containsString(method.getAnnotations(), "@Autowired(required = false)"));
+						// add Optional import if needed
+						optionalImportNeeded = optionalImportNeeded ? true : optionalNeeded;
+						buildConstructor(constructorDeclaration, method.getParameters(), method.getBody().orElse(new BlockStmt()).getStatements(),
+								optionalNeeded);
+						// save first setter to replace it with the constructor
+						if (firstSetter == null) {
+							firstSetter = method;
+						} else {
+							nodesToRemove.add(method);
+						}
+					}
+				}
+			}
+			if (firstSetter != null) {
+				// replace the first saved setter with the constructor
+				firstSetter.replace(constructorDeclaration);
+				// remove the no longer needed setter
+				nodesToRemove.stream().forEach(Node::removeForced);
+			}
+		}
+		if (optionalImportNeeded) {
+			cu.addImport(Optional.class);
+		}
+	}
 
-        // prints the changed compilation unit
-        System.out.println(cu.toString());
-    }
+	// TODO this can be done more elegant using the elements of the AnnotationExpr -> replacing the string with an AnnotationExpr
+	private static boolean containsString(final NodeList<AnnotationExpr> nodeList, final String toBeContained) {
+		if (nodeList == null) {
+			return false;
+		}
+		for (final AnnotationExpr node : nodeList) {
+			if (node.toString().contains(toBeContained)) {
+				return true;
+			}
+		}
+		return false;
+	}
 
-    private static void changeToConstructorInjection(CompilationUnit cu) {
-        boolean optionalImportNeeded = false;
-        List<MethodDeclaration> nodesToRemove = new ArrayList<>();
-        // Go through all the types in the file
-        NodeList<TypeDeclaration<?>> types = cu.getTypes();
-        for (TypeDeclaration<?> type : types) {
-            // Set Type and Autowired for the constructor
-            Node firstSetter = null;
-            ConstructorDeclaration constructorDeclaration = new ConstructorDeclaration();
-            constructorDeclaration.setName(type.getName());
-            constructorDeclaration.addAnnotation("Autowired");
+	//TODO is another treatment of Collections with required false correct needed?
+	/**
+	 * Adds the contents of a method to an existing ConstructorDeclaration
+	 * 
+	 * @param constructorDeclaration
+	 * @param setterParameters should be of size 1 -> otherwise assertion will fail
+	 * @param setterBody
+	 * @param makeOptional when the previous setter Annotation had required = false make the value optional
+	 * @throws Exception
+	 */
+	private static void buildConstructor(final ConstructorDeclaration constructorDeclaration, final NodeList<Parameter> setterParameters,
+			final NodeList<Statement> setterBody, final boolean makeOptional) throws Exception {
+		assert setterParameters.size() == 1;
+		final Parameter parameter = setterParameters.get(0);
 
-            NodeList<BodyDeclaration<?>> members = type.getMembers();
-            for (BodyDeclaration<?> member : members) {
-                if (member instanceof MethodDeclaration) {
-                    MethodDeclaration method = (MethodDeclaration) member;
-                    // only go for annotated methods
-                    if (containsString(method.getAnnotations(), "@Autowired")) {
-                        // special treatment for non required autowirings
-                        boolean optionalNeeded = (containsString(method.getAnnotations(), "@Autowired(required = false)"));
-                        // add Optional import if needed
-                        optionalImportNeeded = optionalImportNeeded ? true : optionalNeeded;
-                        buildConstructor(constructorDeclaration, method.getParameters(), method.getBody().orElse(new BlockStmt()).getStatements(), optionalNeeded);
-                        // save first setter to replace it with the constructor
-                        if (firstSetter == null) {
-                            firstSetter = method;
-                        } else{
-                            nodesToRemove.add(method);
-                        }
-                    }
-                }
-            }
-            // replace the first saved setter with the constructor
-            firstSetter.replace(constructorDeclaration);
-            // remove the no longer needed setter
-            nodesToRemove.stream().forEach(Node::removeForced);
-        }
-        if(optionalImportNeeded){
-            cu.addImport(Optional.class);
-        }
-    }
-
-    // TODO this can be done more elegant using the elements of the AnnotationExpr
-    private static boolean containsString(NodeList<AnnotationExpr> nodeList, String toBeContained) {
-        if (nodeList == null) {
-            return false;
-        }
-        for (AnnotationExpr node : nodeList) {
-            if (node.toString().contains(toBeContained)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    //TODO is treatment of Collections with required false correct?
-    /**
-     * Adds the contents of a method to an existing ConstructorDeclaration
-     * @param constructorDeclaration
-     * @param setterParameters should be of size 1 -> otherwise assertion will fail
-     * @param setterBody
-     * @param makeOptional when the previous setter Annotation had required = false make the value optional
-     */
-    private static void buildConstructor(ConstructorDeclaration constructorDeclaration, NodeList<Parameter> setterParameters, NodeList<Statement> setterBody, boolean makeOptional) {
-        assert setterParameters.size() == 1;
-        Parameter parameter = setterParameters.get(0);
-
-        // add the Parameter
-        String optionalVarName = null;
-        if (makeOptional) {
-            String typeOfNonRequired = parameter.getType().toString();
-            optionalVarName = parameter.getNameAsString();
-            constructorDeclaration.addParameter("Optional<" + typeOfNonRequired + ">", optionalVarName);
-        } else {
-            constructorDeclaration.addParameter(parameter);
-        }
-        // add the body to the constructor's body
-        BlockStmt body = constructorDeclaration.getBody();
-        for (Statement statement : setterBody) {
-            // make parameter optional if makeOptional == true
-            //TODO there are other Expressions which might be also needed -> e.g. VariableDeclarations fail here
-            if (makeOptional && statement.isExpressionStmt()) {
-                Expression expression = ((ExpressionStmt) statement).getExpression();
-                if (expression.isAssignExpr()) {
-                    Expression valueExpression = ((AssignExpr) expression).getValue();
-                    if (valueExpression.isNameExpr()) {
-                        String value = ((NameExpr) valueExpression).getNameAsString();
-                        if (value.equals(optionalVarName)) {
-                            value += ".orElse(null)";
-                            ((NameExpr) valueExpression).setName(value);
-                        }
-                    }
-                }
-            }
-            body.addStatement(statement);
-        }
-    }
+		// add the Parameter
+		String optionalVarName = null;
+		if (makeOptional) {
+			final String typeOfNonRequired = parameter.getType().toString();
+			optionalVarName = parameter.getNameAsString();
+			constructorDeclaration.addParameter("Optional<" + typeOfNonRequired + ">", optionalVarName);
+		} else {
+			constructorDeclaration.addParameter(parameter);
+		}
+		// add the body to the constructor's body
+		final BlockStmt body = constructorDeclaration.getBody();
+		boolean isOptionalUsed = false;
+		for (final Statement statement : setterBody) {
+			// make parameter optional if makeOptional == true
+			//TODO there are other Expressions which might be also needed -> e.g. VariableDeclarations fail here
+			if (makeOptional) {
+				try {
+					// only use NameExpr at the time, ignore everything else
+					final NameExpr nameExpr = statement.asExpressionStmt().getExpression().asAssignExpr().getValue().asNameExpr();
+					String value = nameExpr.getNameAsString();
+					if (value.equals(optionalVarName)) {
+						value += ".orElse(null)";
+						nameExpr.setName(value);
+						isOptionalUsed = true;
+					}
+				} catch (final IllegalStateException e) {
+					// nothing todo, the Expression is simply not of intrest
+				}
+			}
+			body.addStatement(statement);
+		}
+		if (makeOptional && !isOptionalUsed) {
+			throw new Exception("Optional wasn't used");
+		}
+	}
 }
